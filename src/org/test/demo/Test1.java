e8720db8c032e860e7fe5cf1e95cb7056a4d3f25
day <laasiya.sivanandam@gmail.com>
2024-07-31 07:46:24 +0530

    new

---
+++ src/org/test/demo/Test1.java
@@ -0,0 +1,12 @@
+package org.test.demo;
+
+public class Test1 {
+private void cmpId() {
+	System.out.println("company id id 123");
+
+}
+public static void main(String[] args) {
+	Test1 t =new Test1();
+	t.cmpId();
+}
+}
